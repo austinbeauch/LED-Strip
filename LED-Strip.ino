41215f270d236404216507afcd09bc49a8936fb2
Austin <YOUR EMAIL ADDRESS>
2017-03-05 23:44:59 -0800

    updated comments

--- LED-Strip.ino
+++ LED-Strip.ino
@@ -1,9 +1,12 @@
 #include <IRremote.h>
+
+//pin numbers
 const static int GREEN = 9;
 const static int RED = 5;
 const static int BLUE = 6;
 const static int RECV_PIN = 11;
 
+//Remote button values. Change these depending on remote
 const static double one = 0xFF30CF; //green
 const static double two = 0xFF18E7; //red
 const static double three = 0xFF7A85; //blue
@@ -15,6 +18,7 @@ const static double minus = 0xFFE01F; //brightness down
 int test;
 int brightness = 255;
 
+//initial state of LEDs
 bool green = false;
 bool red = false;
 bool blue = false;
@@ -46,7 +50,7 @@ void on(){
 void x123(int brightness){
   if(results.value == one){
     green = !green;
-    green ? analogWrite(GREEN, brightness) : digitalWrite(GREEN,LOW);
+    green ? analogWrite(GREEN, brightness) : digitalWrite(GREEN,LOW); //turning on or off depending on state
   }
   else if(results.value == two){
     red = !red;
@@ -67,6 +71,7 @@ void updateBrightness(int brightness){
 void music(){
 
   //TODO: Figure out why turning on any of the lights increases the read values (voltage from 12v psu might be interfering)
+  //TODO: Wait for multimeter to show up so I can test voltages
   
   int volume = digitalRead(13);
   Serial.println(volume);
@@ -84,26 +89,24 @@ void music(){
 void cycleRGB(){
 
   //TODO: Work around the delay to accept more IR signals while cycling 
-  
+  //  Remove for loop and use loop functionality of the Arduino potentially
   unsigned int rgbColour[3];
 
   rgbColour[0] = 255;
   rgbColour[1] = 0;
   rgbColour[2] = 0;  
 
+  //TODO: Remove these loops but maintain their functionality through the main system looping
   for (int decColour = 0; decColour < 3; decColour++) {
-    
     int incColour = decColour == 2 ? 0 : decColour + 1;
-
     for(int i = 0; i < 255; i++) {   
       rgbColour[decColour] -= 1;
       rgbColour[incColour] += 1;
       setColourRgb(rgbColour[0], rgbColour[1], rgbColour[2]);
-      delay(5);
+      delay(5); //right here is where most time is spent in a dead-state so receiving another button press doesn't work
     }
   }
 }
-
 
 void setColourRgb(unsigned int red, unsigned int green, unsigned int blue) {
   analogWrite(RED, red);
@@ -117,7 +120,7 @@ void loop() {
     //TODO: Add a function to cycle whatever colours are currently turned on
     //TODO: Fix music() function
 
-    if(irrecv.decode(&results)){
+    if(irrecv.decode(&results)){ //if button has been pressed
              
       //Serial.println(results.value, HEX);
       
@@ -136,7 +139,7 @@ void loop() {
       }
       
       else if(results.value == four){  
-        if(test!=4) test=4; 
+        if(test!=4) test=4; //setting variable to maintain loop after button press
         else {
           test=0;
           updateBrightness(brightness);
@@ -153,7 +156,7 @@ void loop() {
       }
       irrecv.resume();
     }
-    Serial.println(test);
+    Serial.println(test); //test line to see if function has changed
     if(test==4) music();
     else if(test==5) cycleRGB();
 }
